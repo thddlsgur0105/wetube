d0d5c21fab0ff8d569e041aa52814b3ef7c441a3
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-11 22:05:10 +0900

    Started learning Routes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from Home daniel!!");
-const handleProfile = (req, res) => res.send("You are on my profile.");
 
 app.use(cookieParser());
 app.use(express.json());
@@ -15,8 +15,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
