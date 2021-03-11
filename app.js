65d163163e51420531ae10efc7c1a295aa196506
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-11 16:18:13 +0900

    Started learning Routing

--- app.js
+++ app.js
@@ -2,33 +2,21 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-// import bodyParser from "body-parser";
-
+import { userRouter } from "./router";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-
 const handleHome = (req, res) => res.send("Hello from Home daniel!!");
-
-
 const handleProfile = (req, res) => res.send("You are on my profile.");
 
 app.use(cookieParser());
-// app.use(express.bodyParser.json());
-// app.use(express.bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(morgan("dev"));
 app.use(helmet());
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
+app.use("/user", userRouter);
 
-app.listen(PORT, handleListening);
-// app.get('/', function(req, res) {
-//   res.send('hello world');
-// });
+export default app;
