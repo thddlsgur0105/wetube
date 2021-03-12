78aae648d43cc59656b1e3f755bcfedafafce71f
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-12 15:05:34 +0900

    Finished MVC__Controller

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(morgan("dev"));
 app.use(helmet());
+app.use(morgan("dev"));
+
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
