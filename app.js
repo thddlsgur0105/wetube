4121f982cca8f6ae07b91043a64ae188fbd7e281
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-15 15:19:24 +0900

    Created middlewares.js for using globally

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -9,13 +10,14 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
 
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
