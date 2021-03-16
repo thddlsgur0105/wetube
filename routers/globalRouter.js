13618d44b7238b91f9b423bf1e2ef71baac5c406
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-16 15:45:22 +0900

    Rendering Info to Model by using get method

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -3,6 +3,7 @@ import { join, login, logout } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 import routes from "../routes";
 
+// 상수에 메소드를 넣어 준다? 이해 잘 안감..
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
