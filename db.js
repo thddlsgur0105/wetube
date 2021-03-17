b36665da9a508e272e64404c31d234027ea53452
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-17 10:26:28 +0900

    Rendered fake data to Home template

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        cleator: {
+            id: 123123,
+            name: "Daniel",
+            email: "Dan@iel.com"
+        }
+    },
+    {
+        id: 121212,
+        title: "Video super",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        cleator: {
+            id: 123123,
+            name: "Daniel",
+            email: "Dan@iel.com"
+        }
+    },
+    {
+        id: 555555,
+        title: "Video nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        cleator: {
+            id: 123123,
+            name: "Daniel",
+            email: "Dan@iel.com"
+        }
+    },
+    {
+        id: 111111,
+        title: "Video perfect",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        cleator: {
+            id: 123123,
+            name: "Daniel",
+            email: "Dan@iel.com"
+        }
+    }
+]
