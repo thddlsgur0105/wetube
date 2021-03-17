dbcc86b8cc4d5c474de200d849af386b6720d26c
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-18 01:47:28 +0900

    Updated more Controllers' functionality

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos as videoList } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
     res.render("home", { pageTitle: "Home", videoList });
@@ -8,7 +9,16 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy, videoList });
 };
 export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+    const {
+        body : { file, title, description }
+    } = req;
+    // To Do : Upload and save Video
+    res.redirect(routes.videoDetail(324393));
+};
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
