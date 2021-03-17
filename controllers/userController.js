97ccd0178501ebd90713ab386aac4c0f60abb115
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-17 23:20:36 +0900

    Created functions performing multiple tasks in one way

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,11 @@ export const postJoin = (req, res) => {
     
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home)
+}
+
 export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getJoin, postJoin, login, logout } from "../controllers/userController";
+import { getJoin, postJoin, logout, getLogin, postLogin } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 import routes from "../routes";
 
@@ -11,7 +11,10 @@ globalRouter.post(routes.join, postJoin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
