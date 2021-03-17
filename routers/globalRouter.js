27c7baad6c2ea9db17eb2671142f0e5ec2e8702c
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-17 19:48:24 +0900

    Created getJoin & postJoin in userController

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,16 @@
 import express from "express";
-import { join, login, logout } from "../controllers/userController";
+import { getJoin, postJoin, login, logout } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 import routes from "../routes";
 
 // 상수에 메소드를 넣어 준다? 이해 잘 안감..
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
