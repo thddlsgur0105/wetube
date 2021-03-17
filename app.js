0f9ce948c7019ed47b1771d8accc1f8ec21bbb24
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-17 16:29:58 +0900

    Created mixin function

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import routes from "./routes";
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(express.json());
