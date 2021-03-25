5e68b1740d9441f57b508640bbcb51221bf3df53
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-25 21:55:05 +0900

    Webpack Configuration

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
