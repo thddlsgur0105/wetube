e27d80ca8f959888e7e3c269619e38f4fe5a18b1
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-30 15:59:18 +0900

    GitHub Login -- part 3

--- passport.js
+++ passport.js
@@ -14,5 +14,15 @@ passport.use(
     githubLoginCallback)
 );
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
