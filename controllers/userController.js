ae00c978371f455bd49d20f037f1113c9bc20d8b
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-04-03 18:26:30 +0900

    Created Play Pause Btn

--- controllers/userController.js
+++ controllers/userController.js
@@ -97,8 +97,9 @@ export const logout = (req, res) => {
     res.redirect(routes.home);
 };
 
-export const getMe = (req, res) => {
-    res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+export const getMe = async (req, res) => {
+    const user = await User.findById(req.user.id).populate("videos");
+    res.render("userDetail", { pageTitle: "User Detail", user });
 }
 
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
