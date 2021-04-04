166b5f8e2c808bad62663ea451e1ede88554c17c
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-04-04 11:49:39 +0900

    Created fullScrn Btn

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumnBtn = document.getElementById("jsVolumnBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
     if(videoPlayer.paused) {
@@ -23,9 +24,23 @@ function handleVolumnClick() {
     }
 }
 
+function exitFullScreen() {
+    document.webkitExitFullscreen();
+    fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+    fullScrnBtn.addEventListener("click", goFullScreen);
+}
+
+function goFullScreen() {
+    videoContainer.webkitRequestFullscreen();
+    fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+    fullScrnBtn.removeEventListener("click", goFullScreen);
+    fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
 function init() {
     playBtn.addEventListener("click", handlePlayClick);
     volumnBtn.addEventListener("click", handleVolumnClick);
+    fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
