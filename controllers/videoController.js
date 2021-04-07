076dba10bec32536d1e403f913a78d9500501560
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-04-07 22:33:29 +0900

    Comment Adding API -- backend

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async(req, res) => {
     try {
@@ -45,7 +46,7 @@ export const videoDetail = async(req, res) => {
         params : {id}
     } = req;
     try {
-        const video = await Video.findById(id).populate("creator");
+        const video = await (await Video.findById(id).populate("creator")).populate("comments");
         res.render("videoDetail", { pageTitle: video.title, video })
     } catch(error) {
         console.log(error);
@@ -108,6 +109,29 @@ export const postRegisterView = async (req, res) => {
         res.status(200);
     } catch (error) {
         res.status(400);
+    } finally {
+        res.end();
+    }
+}
+
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+    const {
+        params: { id },
+        body: { comment },
+        user
+    } = req;
+    try {
+        const video = await Video.findById(id);
+        const newComment = await Comment.create({
+            text: comment,
+            creator: user.id
+        })
+        video.comments.push(newComment.id);
+        video.save();
+    } catch (error) {
+      res.status(400);  
     } finally {
         res.end();
     }
