38efa3e1c521d804d4e3d4945234d6ad75033227
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-04-09 20:03:17 +0900

    Comment Adding API -- backend & Frontend

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -46,7 +46,7 @@ export const videoDetail = async(req, res) => {
         params : {id}
     } = req;
     try {
-        const video = await (await Video.findById(id).populate("creator")).populate("comments");
+        const video = await Video.findById(id).populate("creator").populate("comments");
         res.render("videoDetail", { pageTitle: video.title, video })
     } catch(error) {
         console.log(error);
